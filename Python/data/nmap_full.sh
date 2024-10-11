77d0357ebf7bb5a7f1bbad947612d000cc381dc1
kirikra <126892040+irikra@users.noreply.github.com>
2024-10-11 18:43:33 +0300

    Delete Python directory

--- Python/data/nmap_full.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Введи IP-адрес или имя хоста: " target
-
-# Выполним сканирование с помощью nmap
-echo "Сканирую $target с опцией -sV..."
-nmap -sV $target
--- Python/data/nmap_vuln.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Введите IP-адрес или имя хоста: " target
-
-# Выполним сканирование с помощью nmap
-echo "Сканируем $target с опцией --vuln"
-nmap -sV --script vuln $target
